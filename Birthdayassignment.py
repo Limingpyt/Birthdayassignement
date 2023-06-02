55e003c7abe9a21112f6e7045ee53b1c2f97d6a8
Limingpyt <134782667+Limingpyt@users.noreply.github.com>
2023-06-02 13:01:15 +0100

    Add files via upload

---
+++ Birthdayassignment.py
@@ -0,0 +1,19 @@
+a = "uncle"
+print (f"Dear {a},")
+print ("let's celebrate your")
+date = 2023
+bdate = 1948
+print (date-bdate)
+print ("years of awesomess!")
+
+age = 75
+after_conv = int(age)
+print (type(after_conv))
+print ("Wishing you a day filled with joy " + "and laughter as you turn " + str(age)+".")
+
+pers_message = "I hope you like the cake I made for you"
+print (f"{pers_message}!")
+
+print ("With love and best wishes,")
+sender_name = "Liming"
+print (f"{sender_name}")
